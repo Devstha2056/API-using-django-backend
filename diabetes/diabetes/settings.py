eebdca3616829e626f281cc2e3c69e3d1da68351
Devendra <shresthadevendra2056@gmail.com>
2024-02-20 09:36:42 +0545

    dev

--- diabetes/diabetes/settings.py
+++ diabetes/diabetes/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
      'corsheaders',
      'rest_framework',
+
      
 ]
 
@@ -131,3 +132,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #aa
 CORS_ALLOW_ALL_ORIGINS=True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React development server
+]
